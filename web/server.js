9e7245f648762c4c4b9ec732682b9dabf5d7854b
YFLooi <yihfoo@gmail.com>
2020-12-03 15:19:53 +0800

    corrected route on load to target index.html

--- web/server.js
+++ web/server.js
@@ -8,8 +8,12 @@ require('dotenv').config(); //Required to access .env files
 app.use(express.static(__dirname+'/web'));
 
 app.get('/', function(req,res){
-	console.log('Main page loading...');
-	res.render('./client/index.html');
+  console.log('Main page loading...');
+  
+  if (error) {
+    throw error
+  }
+  response.sendFile(__dirname + '/client/public/index.html');
 });
 
 app.get("/server/testGet", testResp)
@@ -17,11 +21,13 @@ async function testResp (request, response) {
   console.log('Request for data received by Express backend');
   response.status(200).json("String sent by Express backend");
 }
-/** 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './dist','index.html'));
-});
-*/
+
+//Put this last among all routes. Otherwise, it will return HTML to all fetch requests and trip up CORS. They interrupt each other
+// For any request that doesn't match, this sends the index.html file from the client. This is used for all of our React code.
+//Eliminates need to set redirect in package.json at start script with concurrently
+app.get('*', (req, res) => {  
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
 
 //App will run on process.env.PORT by default. Must specify or Heroku uses its default port
 //It runs on port 4000 only if process.env.PORT is not defined
